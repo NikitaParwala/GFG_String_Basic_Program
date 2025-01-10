5087599c0e8359ca403cf1acecd32a38ec148d5f
Nisha <Nisha@DESKTOP-HDO2FJ2>
2025-01-10 04:21:33 -0800

    6th commit >> modified this problem

--- GFG_String_basic/src/GFG/count_of_camelCase_char.java
+++ GFG_String_basic/src/GFG/count_of_camelCase_char.java
@@ -23,3 +23,25 @@ public class count_of_camelCase_char {
 	}
 
 }
+
+/*
+
+Given a string. Count the number of Camel Case characters in it.
+
+Example 1:
+
+Input:
+S = "ckjkUUYII"
+Output: 5
+Explanation: Camel Case characters present:
+U, U, Y, I and I.
+
+â€‹Example 2:
+
+Input: 
+S = "abcd"
+Output: 0
+Explanation: No Camel Case character
+present.
+
+*/
--- GFG_String_basic/src/GFG/sort_string.java
+++ GFG_String_basic/src/GFG/sort_string.java
@@ -37,3 +37,27 @@ public class sort_string {
 	}
 
 }
+
+
+/*
+
+
+Given a string consisting of lowercase letters, arrange all its letters in ascending order. 
+
+Example 1:
+
+Input:
+S = "edcab"
+Output: "abcde"
+Explanation: characters are in ascending
+order in "abcde".
+Example 2:
+
+Input:
+S = "xzy"
+Output: "xyz"
+Explanation: characters are in ascending
+order in "xyz".
+
+
+*/
